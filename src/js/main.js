808ae7cdf1acaaffae077e1f239d4ae74424ab90
shimanamisan <mikan.sup.all@gmail.com>
2020-09-21 08:15:41 +0900

    スタイリングを修正

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,39 @@
+// ライブラリを読み込み
 const $ = require("jquery");
+import 'particles.js/particles';
+const particlesJS = window.particlesJS;
 
-$(function(){
-    console.log('jQueryを読み込んでいます。。。')
-})
+particlesJS.load('particles-js', '../particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+$(function () {
+
+  let $menu = $(".js-menu");
+  let $nav_bg = $(".js-nav-background");
+  let $nav = $(".js-nav");
+  $menu.on("click", function () {
+    // アロー関数だとthisが束縛されないので注意
+    $nav_bg.toggleClass("p-header__nav__background--active");
+    $(this).toggleClass("burger--active"); // アロー関数にしていると、ここのthisがdocumentオブジェクトが指定される
+    $nav.toggleClass("p-header__nav--active");
+  });
+
+  // リンク内のスムーズスクロール
+  // #で始まる要素をクリックした場合に処理を実行
+  $('a[href^="#"]').on("click", function () {
+    $nav_bg.toggleClass("p-header__nav__background--active");
+    $menu.toggleClass("burger--active"); // アロー関数にしていると、ここのthisがdocumentオブジェクトが指定される
+    $nav.toggleClass("p-header__nav--active");
+    let href = $(this).attr("href"),
+      target = $(href == "#" || href == "" ? "html" : href),
+      position = target.offset().top;
+    $("body,html").animate(
+      {
+        scrollTop: position,
+      },
+      0
+    );
+    return false; // aタグの挙動を無効化する
+  });
+});
