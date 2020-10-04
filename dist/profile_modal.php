5c4f6d5994388cf4ffcda3cad1fd390df7a0408d
shimanamisan <mikan.sup.all@gmail.com>
2020-10-04 19:21:28 +0900

    workモーダルとプロフィールモーダルの途中まで

--- dist/profile_modal.php
+++ dist/profile_modal.php
@@ -1,3 +1,10 @@
-<!-- profile detail modal -->
-
+ <!-- profile detail modal -->
+ <div class="c-modal js-profile-modal">
+    <div class="c-modal__bg js-modal__bg"></div>
+    <div class="c-modal__container">
+        <div class="c-modal__body">
+            <div class="c-modal__body__inner"></div>
+        </div>
+    </div>
+</div>
 <!-- end profile detail modal -->
