cf6b829d849c954cfa677febf243e24ba41e4f3c
shimanamisan <mikan.sup.all@gmail.com>
2020-10-16 07:56:55 +0900

    紹介ページを編集

--- dist/work_modal/crypto_trend.php
+++ dist/work_modal/crypto_trend.php
@@ -15,23 +15,24 @@
                     <div class="c-modal__site__detail">
                         <span>内容</span>
                         <p>
-                            スクールの卒業課題として作成したWebサービスです。
-                            提案からデザイン、ロゴデザイン、文章作成、コーディング、実装まで1人で担当。
+                            Twitterを利用した仮想通貨トレンド分析サービスです。プログラミングスクールの卒業試験として提示された要件を元に、
+                            画面設計、DB設計、コーディング、デプロイまで1人で行いました。
                         </p>
                         <p>
-                            他のチームと差別化出来るようにとにかく目立つサイトにして欲しい、という要望を意識しつつ訪問者の目を引けるように背景に動画を使用しました。
-                            また、ターゲット層が男性なので格好良さを演出できるよう、ダークなでデザインに仕上げました。
+                        3ヶ月という納期の中で提示された要件以外でも複数機能を実装し、要件の中で不明瞭な箇所は実際にヒアリングして認識のズレがないように心がけました。
+                        サービスを作成すること以外にも、どのくらいの期間で機能が実装できそうか見積もりをする力もつけることが出来ました。
                         </p>
                         <span>機能</span>
                         <p>
-                            動画スライダー / CSSアニメーション / 初回アクセス時ローディングアイコン表示 / お問い合わせ機能
+                            ユーザー登録・ログイン・ログアウト・退会機能 / Twitter認証機能 / パスワードリマインダー / ページネーション機能 / マイページ機能 / メールアドレス認証機能 /
+                            仮想通貨関連ニュース取得（Google News API）/ 仮想通貨関連ユーザー取得（TwitterAPI） / 関連ユーザーフォロー・アンフォロー機能 / 関連ユーザー自動フォロー機能
                         </p>
                     </div>
                     <div class="c-modal__site__info">
-                        <span>URL：</span><p><a href="https://task-app.shimanamisan.com" target="_brank" rel=”noreferrer”>https://task-app.shimanamisan.com</a></p>
+                        <span>URL：</span><p><a href="https://crypto-trend2020.shimanamisan.com" target="_brank" rel=”noreferrer”>https://crypto-trend2020.shimanamisan.com</a></p>
                     </div>
                     <div class="c-modal__site__info">
-                        <span>GitHub：</span><p><a href="https://github.com/shimanamisan/lineup.git" target="_brank" rel=”noreferrer”>ソースコード</a></p>
+                        <span>GitHub：</span><p><a href="https://github.com/shimanamisan/cryptotrend_app.git" target="_brank" rel=”noreferrer”>ソースコード</a></p>
                     </div>
                 </div>
             </div>
