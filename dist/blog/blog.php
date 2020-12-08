7503c85276674b231616054c4bd7edcedbb02955
shimanamisan <mikan.sup.all@gmail.com>
2020-12-08 10:52:02 +0900

    blogテンプレートファイルを作成

---
+++ dist/blog/blog.php
@@ -0,0 +1,44 @@
+<div class="p-skill__wrapp">
+          <div class="p-skill__card u-js-fadeIn">
+            <figure>
+              <img 
+              width="486"
+              height="290"
+              src="https://blog.shimanamisan.com/wp-content/uploads/2020/03/Vagrant_Laravel.png" alt="">
+            </figure>
+            <div>
+              <p>
+              Vagrant + CentOS8 + Laravel で開発環境を構築
+              </p>
+            </div>
+          </div>
+          <div class="p-skill__card u-js-fadeIn">
+            <figure>
+              <img 
+              width="486"
+              height="290"
+              src="https://blog.shimanamisan.com/wp-content/uploads/2020/10/todo_list.png" alt="">
+            </figure>
+            <div>
+              <p>
+              【Vue.jsでTODOリストを作成
+              </p>
+            </div>
+          </div>
+          <div class="p-skill__card u-js-fadeIn">
+            <figure>
+              <a href="https://blog.shimanamisan.com/archives/373
+              ">
+              <img 
+              width="486"
+              height="290"
+              src="https://blog.shimanamisan.com/wp-content/uploads/2020/11/TaskApp.png" alt="">
+            </a>
+            </figure>
+            <div>
+              <p>
+              Vue.js + Laravelでタスク管理アプリを作成した話
+              </p>
+            </div>
+          </div>
+      </div>
