5fc83a988f0bb6097c6e892891006864635f976e
shimanamisan <mikan.sup.all@gmail.com>
2020-12-15 08:03:31 +0900

    [update]ブログカードに新規クラスを追加

--- dist/blog/blog.php
+++ dist/blog/blog.php
@@ -1,44 +1,44 @@
 <div class="p-skill__wrapp">
           <div class="p-blog__card p-skill__card u-js-fadeIn">
-            <figure>
+            <a href="https://blog.shimanamisan.com/archives/238
+            " target="_brank" rel=”noreferrer”>
+            <figure class="p-blog__imgWrapp">
               <img 
-              width="486"
-              height="290"
               src="https://blog.shimanamisan.com/wp-content/uploads/2020/03/Vagrant_Laravel.png" alt="">
             </figure>
-            <div>
+            <div class="p-blog__detail__text">
               <p>
               Vagrant + CentOS8 + Laravel で開発環境を構築
               </p>
             </div>
-          </div>
-          <div class="p-blog__card p-skill__card u-js-fadeIn">
-            <figure>
-              <img 
-              width="486"
-              height="290"
-              src="https://blog.shimanamisan.com/wp-content/uploads/2020/10/todo_list.png" alt="">
-            </figure>
-            <div>
-              <p>
-              【Vue.jsでTODOリストを作成
-              </p>
-            </div>
-          </div>
-          <div class="p-blog__card p-skill__card u-js-fadeIn">
-            <figure>
-              <a href="https://blog.shimanamisan.com/archives/373
-              ">
-              <img 
-              width="486"
-              height="290"
-              src="https://blog.shimanamisan.com/wp-content/uploads/2020/11/TaskApp.png" alt="">
             </a>
+          </div>
+          <div class="p-blog__card p-skill__card u-js-fadeIn">
+            <a href="https://blog.shimanamisan.com/archives/373
+            " target="_brank" rel=”noreferrer”>
+            <figure class="p-blog__imgWrapp">
+              <img 
+              src="https://blog.shimanamisan.com/wp-content/uploads/2020/11/TaskApp.png" alt="">
             </figure>
             <div>
-              <p>
-              Vue.js + Laravelでタスク管理アプリを作成した話
+              <p class="p-blog__detail__text">
+                【SPA】Vue.js + Laravelでタスク管理アプリを作成した話
               </p>
             </div>
+          </a>
+        </div>
+        <div class="p-blog__card p-skill__card u-js-fadeIn">
+          <a href="https://blog.shimanamisan.com/archives/180
+          " target="_brank" rel=”noreferrer” >
+          <figure class="p-blog__imgWrapp">
+            <img 
+            src="https://blog.shimanamisan.com/wp-content/uploads/2020/10/todo_list.png" alt="">
+          </figure>
+          <div>
+            <p class="p-blog__detail__text">
+            Vue.jsでTODOリストを作成
+            </p>
           </div>
+          </a>
+        </div>
       </div>
