1a9e23f4c417ddcebed3e89c95a5a89b0e0c30e5
shimanamisan <mikan.sup.all@gmail.com>
2020-12-15 08:04:13 +0900

    [update] リンク先にBlogセクションを追加

--- dist/header.php
+++ dist/header.php
@@ -12,6 +12,7 @@
         <li class=""><a href="#">HOME</a></li>
         <li class=""><a href="#about">About</a></li>
         <li class=""><a href="#work">Work</a></li>
+        <li class=""><a href="#blog">Blog</a></li>
         <li class=""><a href="#skill">Skill</a></li>
         <li class=""><a href="#contact">Contact</a></li>
       </ul>
